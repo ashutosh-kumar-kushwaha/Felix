7c4ed3c7a0bd3ba2e37bea99d1f129c2b90aa26e
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2022-10-23 22:59:40 +0530

    Added dimen files

--- app/src/main/java/in/silive/felix/SplashActivity.kt
+++ app/src/main/java/in/silive/felix/SplashActivity.kt
@@ -1,10 +1,19 @@
 package `in`.silive.felix
 
+import android.annotation.SuppressLint
+import android.app.ActionBar
 import android.app.Activity
+import android.content.ClipDescription
 import android.content.Intent
+import android.graphics.drawable.ClipDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.support.v4.content.res.ResourcesCompat
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class SplashActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +22,29 @@ class SplashActivity : Activity() {
 
 
         val intent : Intent = Intent(this, LoginActivity::class.java)
+        val felixLL : LinearLayout = findViewById(R.id.felixLogoLL)
+        val felixImages = arrayOf(R.drawable.f, R.drawable.e, R.drawable.l, R.drawable.i, R.drawable.x)
+
+        for(i in 0..4){
+            Handler(Looper.getMainLooper()).postDelayed({
+                val img : ImageView = ImageView(this)
+                img.setImageResource(felixImages[i])
+                val params = LinearLayout.LayoutParams(resources.getDimensionPixelSize(R.dimen.dp_50), resources.getDimensionPixelSize(R.dimen.dp_80))
+                img.layoutParams = params
+                felixLL.addView(img)
+            }, (300 + i*300).toLong())
+        }
+
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 2000)
+
+
+
 
 
     }
 }
+
