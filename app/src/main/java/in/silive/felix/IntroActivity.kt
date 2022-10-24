622caa33ee8a11b7ea6a610cadaf1ec9d025f2f9
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2022-10-25 01:19:14 +0530

    Completed Intro Activity UI

---
+++ app/src/main/java/in/silive/felix/IntroActivity.kt
@@ -0,0 +1,11 @@
+package `in`.silive.felix
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
