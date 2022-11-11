7bcec0b209d576090df0da5f1e1d32add8f36546
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2022-11-11 17:37:03 +0530

    Add Admin Page

--- app/src/main/java/in/silive/felix/AdminFragment.kt
+++ app/src/main/java/in/silive/felix/AdminFragment.kt
@@ -12,8 +12,10 @@ class AdminFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_admin, container, false)
+
+        return view
     }
 
 }
